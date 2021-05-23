8fdbf141ca247f45ad41504716e50d53b95f9449
rafaeltarrendo41 <a89162@alunos.uminho.pt>
2021-05-23 03:03:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -82,6 +82,20 @@ app.get("/pagamentos", (request, response) =>{
     });
 })
 
+app.get("/matchCarga", (request, response) =>{
+    response.set('Content-Type', 'text/html');
+    response.render(`${dirName}/matchCarga`, {
+        urlBase: urlBase,
+        ver: version
+    });
+})
+app.get("/matchTransporte", (request, response) =>{
+    response.set('Content-Type', 'text/html');
+    response.render(`${dirName}/matchTransporte`, {
+        urlBase: urlBase,
+        ver: version
+    });
+})
 
 
 app.listen(process.env.PORT, () => console.log(`Node server listening on port ${process.env.PORT}!`));
